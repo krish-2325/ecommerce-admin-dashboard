f523c7a3d8980709b1b00763f3466a38c3107dc0
krish-2325 <goyalkrish088@gmail.com>
2025-12-18 17:48:47 +0530

    Initialize next js and basic routing

---
+++ app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductsPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Products</h1>
+      <p>All products will be managed here.</p>
+    </main>
+  );
+}
